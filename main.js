2c66c0353b1a9d4e7dbbc7da0a43b968e0428e75
Ian Goller <ian.goller915@gmail.com>
2023-03-27 12:13:47 -0400

    add getPlayerChoice fn

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 let choices = ['rock', 'paper', 'scissors']
 
 function getComputerChoice(){
-    const randomElement = choices[Math.floor(Math.random() * choices.length)];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     console.log(randomElement)
+}
+
+function getPlayerChoice(){
+    const playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+        if (choices.includes(playerChoice)){
+            console.log(playerChoice)
+        } else {console.log('invalid entry')}
 }
