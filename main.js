01ce8ca536bad9412549949285d1209408eded90
Ian Goller <ian.goller915@gmail.com>
2023-03-27 12:39:02 -0400

    add playRound fn

--- main.js
+++ main.js
@@ -2,12 +2,46 @@ let choices = ['rock', 'paper', 'scissors']
 
 function getComputerChoice(){
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(randomElement)
+    console.log(computerChoice)
+    return computerChoice
 }
 
 function getPlayerChoice(){
     const playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
         if (choices.includes(playerChoice)){
-            console.log(playerChoice)
+            return playerChoice
         } else {console.log('invalid entry')}
+}
+
+function playRound() {
+    const playerChoice = getPlayerChoice()
+    const computerChoice = getComputerChoice()
+
+    if (playerChoice == computerChoice) {
+        return "Tie"
+    } 
+
+    if (playerChoice=='rock' && computerChoice=='scissors'){
+        return "Player Wins"
+    }
+
+    if (playerChoice=='rock' && computerChoice=='paper'){
+        return "Computer Wins"
+    }
+
+    if (playerChoice=='paper' && computerChoice=='scissors'){
+        return "Computer Wins"
+    }
+
+    if (playerChoice=='paper' && computerChoice=='rock'){
+        return "Player Wins"
+    }
+
+    if (playerChoice=='scissors' && computerChoice=='rock'){
+        return "Computer Wins"
+    }
+
+    if (playerChoice=='scissors' && computerChoice=='paper'){
+        return "Player Wins"
+    }
 }
